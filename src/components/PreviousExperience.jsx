fc07348600ab5a217f656a242481a494bfcccc3d
Samarth Hiremath <samhiremath@ucdavis.edu>
2024-06-21 17:00:50 -0700

    Formatted for hi-fis in Experience and Projects. Added content to Previous Experience and Project sections.

--- src/components/PreviousExperience.jsx
+++ src/components/PreviousExperience.jsx
@@ -1,67 +1,73 @@
 import React from 'react';
+import SwiftLogo from '../images/Swift-logo.png';
+import OptiwiseLogo from '../images/Optiwise-logo.svg';
+import MiraBrandsLogo from '../images/Mira-Brands-logo.webp';
+import CortexxusLogo from '../images/Cortexxus-logo.png';
 
 const experiences = [
   {
-    title: "Software Engineer",
-    company: "Company A",
-    dates: "Jan 2020 - Dec 2021",
-    description: "Developed web applications using React and Node.js...",
-    logo: "/path-to-company-a-logo.svg",
-    color: "text-green-500",
+    title: "Co-Founder / Project Manager",
+    company: "Swift",
+    dates: "Present",
+    description: "I am leading a cross-functional team in UI/UX framework design and prototype development. I leveraged front-end expertise and project management skills to achieve success in UC Davis entrepreneurship competitions. I coordinate customer testimonials, translate user feedback into technical requirements, and oversee the development process.",
+    logo: SwiftLogo,
+    color: "text-green",
   },
   {
-    title: "Product Manager",
-    company: "Company B",
-    dates: "Jan 2019 - Dec 2019",
-    description: "Managed product lifecycle and cross-functional teams...",
-    logo: "/path-to-company-b-logo.svg",
-    color: "text-blue-500",
+    title: "Software Engineering Intern",
+    company: "Optiwise.ai, Inc",
+    dates: "Jan 2024 - Jun 2024",
+    description: "I developed 5 responsive web pages using React and Node.js, creating reusable components that reduced code redundancy by 30%. I implemented 10 dynamic backend routes with FastAPI and helped develop a scalable SQL database schema. I collaborated with UI/UX designers to build a highly responsive website.",
+    logo: OptiwiseLogo,
+    color: "text-blue",
   },
   {
-    title: "Intern",
-    company: "Company C",
-    dates: "Jun 2018 - Dec 2018",
-    description: "Assisted in software development and testing...",
-    logo: "/path-to-company-c-logo.svg",
-    color: "text-rose-500",
+    title: "UI/UX Consultant",
+    company: "Mira Brands",
+    dates: "Oct 2023 - Dec 2023",
+    description: "I created mobile app mock-ups for a recipe-sharing platform using Figma, designing over 10 pages with dynamic animations. I analyzed competitor and market data, integrating unique features like an ingredient spotlight. I blended user-centric design with market insights to enhance the overall product.",
+    logo: MiraBrandsLogo,
+    color: "text-yellow",
   },
   {
-    title: "Freelancer",
-    company: "Company D",
-    dates: "Jan 2017 - May 2018",
-    description: "Developed various freelance projects...",
-    logo: "/path-to-company-d-logo.svg",
-    color: "text-yellow-500",
+    title: "Machine Learning Engineer Intern",
+    company: "Cortexxus",
+    dates: "Jun 2023 - Sep 2023",
+    description: "I contributed to enhancing a seizure prediction LLM in a remote five-member team. I conducted research on LLM development and optimization for EEG data processing. I implemented robust version control and UNIX-based workflows, reducing code conflicts by 25% and demonstrating proficiency in development operations.",
+    logo: CortexxusLogo,
+    color: "text-rose",
   },
-  {
-    title: "Junior Developer",
-    company: "Company E",
-    dates: "Jan 2016 - Dec 2016",
-    description: "Worked on frontend development...",
-    logo: "/path-to-company-e-logo.svg",
-    color: "text-purple-500",
-  },
+  // {
+  //   title: "Junior Developer",
+  //   company: "Company E",
+  //   dates: "Jan 2016 - Dec 2016",
+  //   description: "Worked on frontend development...",
+  //   logo: "/path-to-company-e-logo.svg",
+  //   color: "text-purple",
+  // },
 ];
 
 const PreviousExperience = () => {
   return (
     <div className="bg-black py-16">
       <h1 className="text-4xl text-white font-bold text-center mb-12">Previous Experience</h1>
-      <div className="px-8 space-y-16">
+      <div className="px-16 space-y-24">
         {experiences.map((exp, index) => (
-          <div key={index} className="flex items-center relative space-x-4">
-            <div className="w-1/3 text-right pr-4">
-              <h3 className={`text-xl font-bold ${exp.color}`}>{exp.company}</h3>
+          <div key={index} className="flex items-center relative space-x-6">
+            <div className="w-1/3 text-right pr-6">
+              <h3 className={`text-2xl font-bold ${exp.color}`}>{exp.company}</h3>
               <p className="text-lightGray italic">{exp.dates}</p>
             </div>
             <div className="w-1/12 flex justify-center relative">
-              <div className="h-16 w-16 rounded-full border-4 border-teal overflow-hidden z-10">
-                <img src={exp.logo} alt={exp.company} className="h-full w-full object-cover" />
+              <div className="h-36 w-36 bg-white p-1 rounded-full overflow-hidden z-10">
+                <img src={exp.logo} alt={exp.company} className="h-full w-full object-contain" />
               </div>
-              <div className={`absolute h-full w-1 bg-${exp.color} top-0 left-1/2 transform -translate-x-1/2`} />
+              {index < experiences.length - 1 && (
+                <div className="absolute h-full w-1 bg-white top-full left-1/2 transform -translate-x-1/2" />
+              )}
             </div>
-            <div className="w-1/2 pl-4">
-              <h2 className="text-xl text-white font-bold">{exp.title}</h2>
+            <div className="w-1/2 pl-6">
+              <h2 className="text-lg text-white font-bold">{exp.title}</h2>
               <p className="text-lightGray">{exp.description}</p>
             </div>
           </div>
@@ -72,6 +78,10 @@ const PreviousExperience = () => {
 };
 
 export default PreviousExperience;
+
+
+
+
 
 
 //________________________________________
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,103 @@
 import React from 'react';
 
-const projects = [
+import funWithFlora from '../images/fun-with-flora.png';
+import mentalHealthConnect from '../images/mentalhealth-connect.png';
+import blackjackAI from '../images/blackjack-ai.jpg';
+import miraBrands from '../images/mira-brands.gif';
+
+const projectsData = [
   {
-    title: "Project One",
-    description: "This is a description of project one.",
-    image: "https://via.placeholder.com/300",
-    demoLink: "#",
-    githubLink: "#",
+    id: 1,
+    image: mentalHealthConnect,
+    title: 'MentalHealth Connect',
+    description: 'Created a community-based social platform for students using Python, Flask, React.js, and MySQL, featuring an ML-powered friend recommendation system based on K-Nearest Neighbors algorithm trained on 10,000 user profiles.',
+    demoLink: 'https://devpost.com/software/mentalwell-connect',
+    githubLink: 'https://github.com/Krigupt/SacHacks',
   },
   {
-    title: "Project Two",
-    description: "This is a description of project two.",
-    image: "https://via.placeholder.com/300",
-    demoLink: "#",
-    githubLink: "#",
+    id: 2,
+    image: funWithFlora,
+    title: 'Fun with Flora',
+    description: 'Developed a web application that uses machine learning and APIs to identify plants from images taken by any device, assess their health, and suggest treatments for potential diseases.',
+    demoLink: 'https://docs.google.com/document/d/1fBNoox80M9wddRwBO5LabIjj1A1XV9YMsZLazBGmEs4/edit?usp=sharing',
+    githubLink: 'https://github.com/Samarth-Hiremath1/Fun-With-Flora',
   },
   {
-    title: "Project Three",
-    description: "This is a description of project three.",
-    image: "https://via.placeholder.com/300",
-    demoLink: "#",
-    githubLink: "#",
+    id: 3,
+    image: blackjackAI,
+    title: 'Blackjack AI',
+    description: 'An innovative blackjack bot leveraging Q-learning to optimize gameplay strategies for the Google Developer Student Club @ UC Davis.',
+    demoLink: 'https://docs.google.com/presentation/d/1GdYHb8CVW-bAWbQH8Qt3tYmktVt_v_BQjTn2lCsTA6Y/edit?usp=sharing',
+    githubLink: 'https://github.com/hanyiliu/SamarthBlackjackBot',
   },
   {
-    title: "Project Four",
-    description: "This is a description of project four.",
-    image: "https://via.placeholder.com/300",
-    demoLink: "#",
-    githubLink: "#",
+    id: 4,
+    image: miraBrands,
+    title: 'Mira Brands Recipe App',
+    description: 'Designed a culinary-focused social platform where users can create, share, discover, and rate recipes within a community of food enthusiasts.',
+    demoLink: '#',
+    githubLink: '#',
   },
 ];
 
+// ProjectCard component
+const ProjectCard = ({ project }) => {
+  return (
+    <div className="relative group transform hover:scale-105 transition duration-300 bg-gray-800 rounded-lg overflow-hidden">
+      {/* Image */}
+      <img
+        src={project.image}
+        alt={project.title}
+        className="w-full h-68 object-cover" // Increased height
+      />
+      {/* Content */}
+      <div className="p-4">
+        <h2 className="text-2xl text-white font-bold mb-2">{project.title}</h2>
+        <p className="text-lightGray mb-4">{project.description}</p>
+        <br></br>
+        <br></br>
+        {/* Buttons */}
+        {project.id !== 4 && (
+          <div className="absolute bottom-1 right-4 flex space-x-4 justify-between p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <a
+              href={project.demoLink}
+              className="bg-teal-600 text-white py-0.5 px-4 rounded-lg"
+            >
+              Demo
+            </a>
+            <a
+              href={project.githubLink}
+              className="bg-teal-600 text-white py-0.5 px-4 rounded-lg"
+            >
+              GitHub
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+// Projects component
 const Projects = () => {
   return (
-    <div className="bg-black py-16">
+    <div className="max-w-7xl mx-auto px-4 py-8">
       <h1 className="text-4xl text-white font-bold text-center mb-12">Projects</h1>
-      <p className="text-lightGray text-center mb-8">Some projects I've worked on over the past few years.</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 px-8 mx-48">
-        {projects.map((project, index) => (
-          <div key={index} className="mt-12 relative group transform hover:scale-105 transition duration-300 bg-gray-800 rounded-lg overflow-hidden">
-            <img src={project.image} alt={project.title} className="w-full h-48 object-cover" />
-            <div className="p-10 "> {/* Increased padding from p-4 to p-6 */}
-              <h2 className="text-2xl text-white font-bold mb-2">{project.title}</h2>
-              <p className="text-lightGray mb-4">{project.description}</p>
-
-              <div className="absolute bottom-1 right-4 flex space-x-4 justify-between p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <a href={project.demoLink} className="bg-teal-600 text-white py-0.5 px-4 rounded-lg">Demo</a>
-                <a href={project.githubLink} className="bg-teal-600 text-white py-0.5 px-4 rounded-lg">GitHub</a>
-              </div>
-            </div>
-          </div>
-        ))}
+      <p className="text-lightGray italic text-center mb-20">Some projects I worked on over the past few years</p>
+      {/* Grid layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 lg:px-32">
+        <div className="md:col-span-1 md:row-start-1 relative">
+          <ProjectCard project={projectsData[0]} />
+        </div>
+        <div className="md:col-start-2 md:row-start-1 relative" style={{ marginTop: '12rem' }}>
+          <ProjectCard project={projectsData[1]} />
+        </div>
+        <div className="md:col-span-1 md:row-start-2 relative" style={{ marginTop: '0rem' }}>
+          <ProjectCard project={projectsData[2]} />
+        </div>
+        <div className="md:col-start-2 md:row-start-2 relative" style={{ marginTop: '12rem' }}>
+          <ProjectCard project={projectsData[3]} />
+        </div>
       </div>
     </div>
   );
